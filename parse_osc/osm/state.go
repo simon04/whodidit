c76f5d6e5b497ef5a9b577750b7a2c191be86b13
Simon Legner <Simon.Legner@gmail.com>
2020-12-26 14:50:51 +0100

    Go: local state, iterate

--- parse_osc/osm/state.go
+++ parse_osc/osm/state.go
@@ -3,40 +3,66 @@ package osm
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"os"
 	"strconv"
 	"strings"
 )
 
 const REPLICATION_SERVER = "https://planet.openstreetmap.org/replication"
+const stateFile = "./whodidit-state.txt"
 
-func GetServerState() (int, error) {
+func WriteLocalState(state int64) {
+	f, err := os.Create(stateFile)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+	_, err = fmt.Fprintf(f, "sequenceNumber=%d", state)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func GetLocalState() int64 {
+	f, err := os.Open(stateFile)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+	return parseState(f)
+}
+
+func GetServerState() int64 {
 	url := REPLICATION_SERVER + "/minute/state.txt"
 	log.Printf("Fetching %s...", url)
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
-		return -1, err
+		panic(err)
 	}
 	req.Header.Set("User-Agent", "whodidit")
 
 	res, err := http.DefaultClient.Do(req)
 	if err != nil {
-		return -1, err
+		panic(err)
 	}
 	defer res.Body.Close()
+	return parseState(res.Body)
+}
 
-	scanner := bufio.NewScanner(res.Body)
+func parseState(reader io.Reader) int64 {
+	scanner := bufio.NewScanner(reader)
 	scanner.Split(bufio.ScanLines)
-	var serverState int
 	for scanner.Scan() {
 		text := scanner.Text()
 		if !strings.HasPrefix(text, "sequenceNumber=") {
 			continue
 		}
 		numberString := text[len("sequenceNumber="):]
-		serverState, _ = strconv.Atoi(numberString)
-		return serverState, nil
+		serverState, _ := strconv.ParseInt(numberString, 10, 0)
+		return serverState
 	}
-	return -1, fmt.Errorf("sequenceNumber= not found")
+	panic("sequenceNumber= not found")
 }
